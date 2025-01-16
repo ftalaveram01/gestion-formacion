0a7feda5c2a6b3225844d6ea9e728b5db134b0cb
Luis Arjona Aguilar <luisarjonaaguilar@gmail.com>
2025-01-16 13:40:04 +0100

    Mapear correctamente la entidad Usuario

--- src/main/java/com/viewnext/gestionformacion/business/model/Usuario.java
+++ src/main/java/com/viewnext/gestionformacion/business/model/Usuario.java
@@ -1,14 +1,15 @@
 package com.viewnext.gestionformacion.business.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "USUARIOS")
 public class Usuario {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String password;
