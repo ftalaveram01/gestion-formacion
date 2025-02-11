c41417e795b367cc8e7e3227ba54314876457f7c
jesus <jesusleondiaz07@gmail.com>
2025-02-11 10:35:56 +0100

    Clase curso completa + SQL completo

--- bbdd/gestionformacion.sql
+++ bbdd/gestionformacion.sql
@@ -7,3 +7,23 @@ CREATE TABLE usuario (
 	email VARCHAR(100) UNIQUE NOT NULL,
 	password VARCHAR(20) NOT NULL
 );
+
+CREATE TABLE curso (
+
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	descripcion VARCHAR(500) ,
+	fecha_inicio DATETIME NOT NULL,
+	fecha_fin DATETIME NOT NULL
+
+);
+
+CREATE TABLE usuario_curso (
+
+	id_curso BIGINT NOT NULL,
+	id_usuario INT NOT NULL,
+    PRIMARY KEY (id_curso, id_usuario),
+    FOREIGN KEY (id_curso) REFERENCES curso(id),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id)
+    
+);
--- src/main/java/com/viewnext/gestionformacion/business/model/Curso.java
+++ src/main/java/com/viewnext/gestionformacion/business/model/Curso.java
@@ -3,6 +3,13 @@ package com.viewnext.gestionformacion.business.model;
 import java.util.Date;
 import java.util.List;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,13 +23,24 @@ import lombok.ToString;
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
+@Entity
 public class Curso {
 
+	@Id
 	private Long id;
+	
 	private String nombre;
 	private String descripcion;
+	
+	@ElementCollection
+	@JoinTable(name="usuario_curso", 
+					joinColumns = @JoinColumn(name="id_curso"))
 	private List<Usuario> listaMiembros;
+	
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date fechaInicio;
+	
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date fechaFin;
 	
 }
